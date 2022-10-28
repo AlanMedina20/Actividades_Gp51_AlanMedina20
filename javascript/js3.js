ce482ce88bff1e20f9820d571cd784745492a857
Alan Medina <aisraelmedinarmail.com>
2022-10-28 14:14:56 -0500

    Evidencia 2: 5 paginas de javascript con diferentes funciones

--- javascript/js3.js
+++ javascript/js3.js
@@ -1,7 +1,20 @@
 
-let numero = 4;
+numero = Number(prompt("Ingresa la cantidad de números perfectos a mostrar(1-4):"))
 
-while ()
+document.write("Fueron: ",numero, " numeros")
 
-numeros=6,28,496,8128
-   
+num = 0;
+for(i = 1; num <= numero-1; i++){
+    if(i % 2 == 0){
+        for((j = (i / 2));(j >= 1); j--){
+            if(i % j == 0){
+                enter = enter + j;
+            }
+        }
+        if(enter == i){
+            document.write("<br/> El resultado del numero perfecto es: ",enter);
+            num++
+            }
+    }
+    enter = 0;
+}
--- javascript/js5.js
+++ javascript/js5.js
@@ -54,3 +54,15 @@ function VerImagen(){
     console.log(img);
     Ver(img,imagen);
 }
+
+
+
+let genera = document.getElementById("genera")
+      let numero = document.getElementById("numtab")
+      let lienzo = document.getElementById("tab")
+    
+      genera.addEventListener("click", () => {
+        for(let i = 1; i<=10; i++) {
+          lienzo.innerHTML += `${numero.value} * ${i} = ${numero.value*i} <br />`
+        }
+      })
